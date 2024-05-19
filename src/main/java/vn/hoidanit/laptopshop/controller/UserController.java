bed661e001aaaa6227721cac518db77dd024da93
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2024-05-19 11:10:58 +0700

    using static file, data in static file is public

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,13 +1,18 @@
 package vn.hoidanit.laptopshop.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
-@RestController
+// MVC
+@Controller
 public class UserController {
     private UserService userService;
     
@@ -15,10 +20,29 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "eric.html";
     }
     
     
+    
 }
+
+// REST
+// @RestController
+// public class UserController {
+//     private UserService userService;
+    
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @GetMapping("/")
+//     public String getHomePage() {
+//         return this.userService.handleHello();
+//     }
+    
+    
+// }
